ef869444e70d1575ed3d784090aada89cae16e21
nandeesh-kundar <nandeesh.n99@gmail.com>
2018-06-29 17:40:04 +0530

    About us and projects

--- src/app/client/layout/layout.component.ts
+++ src/app/client/layout/layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  encapsulation:ViewEncapsulation.None
 })
 export class LayoutComponent implements OnInit {
 
