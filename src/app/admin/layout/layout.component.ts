14735a902723acd15fd1988b85ced48ad73b0d4a
shivaTom <shivashankarkulal91@gmail.com>
2018-06-29 16:39:36 +0530

    Dashboard Design

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./layout.component.css']
 })
 export class   LayoutComponent implements OnInit {
-
+ 
   constructor() {
     //console.log()
 
