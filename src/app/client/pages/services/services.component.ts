72f17ad4e22fa033220755bd7fe07e194126f797
nandeesh-kundar <nandeesh.n99@gmail.com>
2018-07-05 09:25:29 +0530

    services page

--- src/app/client/pages/services/services.component.ts
+++ src/app/client/pages/services/services.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { PageService } from '../../../shared/services/page.service';
+import { ServicesService } from '../../../shared/services/services.service';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-
-  constructor() { }
+  img_url=environment.imgUrl;
+  services_section_id=152;
+  services_section_content: Section;
+  servicesList;
+  currentServiceId:undefined;
+  constructor(private _section:PageService, private _sectionservices:ServicesService) { }
 
   ngOnInit() {
+    this._section.getOuterPageSections(this.services_section_id).subscribe(response=>{
+      this.services_section_content=response as Section;
+    })
+    this._sectionservices.getServices().subscribe(response=>{
+      this.servicesList=response;
+    })  
+  }
+
+  setServiceContent(id, index)
+  {
+   
+    this.currentServiceId=id;
+    this.servicesList;  
+    console.log( this.servicesList);
   }
 
 }
+
+export class Section{
+  title: any;
+  properties: any;
+}
+
