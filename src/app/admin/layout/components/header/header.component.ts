39f6c52d3ed1e68018be1cfc50ac2b1176cdebc4
shivaTom <shivashankarkulal91@gmail.com>
2018-07-09 17:34:25 +0530

    data

--- src/app/admin/layout/components/header/header.component.ts
+++ src/app/admin/layout/components/header/header.component.ts
@@ -8,12 +8,34 @@ import { AuthService } from '../../../services/auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  
+  pushRightClass: string = 'push-right';
   constructor( private authServ:AuthService) { }
 
   ngOnInit() {
   }
+
   logout(){
     this.authServ.logout();
   }
+  
+  isToggled(): boolean {
+    const dom: Element = document.querySelector('body');
+    return dom.classList.contains(this.pushRightClass);
+  }
+
+  toggleSidebar() {
+      const dom: any = document.querySelector('body');
+      dom.classList.toggle(this.pushRightClass);
+  }
+
+  rltAndLtr() {
+      const dom: any = document.querySelector('body');
+      dom.classList.toggle('rtl');
+  }
+
+  onLoggedout() {
+      localStorage.removeItem('isLoggedin');
+  }
+
+ 
 }
